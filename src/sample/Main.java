e3567c557fd89f7a2102c6963104522f50f07d17
Leonardo <20030689@itcelaya.edu.mx>
2022-11-22 20:03:30 -0600

    P1

---
+++ src/sample/Main.java
@@ -0,0 +1,85 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+    private VBox vBox;
+    private MenuBar MenuBarras;
+    private Menu Doctores, Pacientes, Cirugias, Habitacion;
+    private MenuItem ingDoc;
+    private MenuItem regrPaciente;
+    private HBox hBox;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        vBox=new VBox();
+        MenuBarras = new MenuBar();
+        //Menus en la barra de menus
+        Doctores = new Menu("Doctores");
+        Pacientes = new Menu("Pacientes");
+        Cirugias = new Menu("Cirugias");
+        Habitacion = new Menu("Habitaciones");
+
+        //Aquí agregamos los botones a la barra de menu
+        MenuBarras.getMenus().addAll(Doctores, Pacientes,Cirugias, Habitacion );
+
+        ingDoc = new MenuItem("Añadir doctores");
+        ingDoc.setOnAction(event -> EventosDoctores(1));
+
+        regrPaciente = new MenuItem("Registrar paciente");
+        regrPaciente.setOnAction(event -> EventosPacientes(1));
+
+
+
+        Doctores.getItems().addAll(ingDoc);
+        Pacientes.getItems().addAll(regrPaciente);
+
+        //Aquí se agrega todo al vBox
+        vBox.getChildren().addAll(MenuBarras);
+
+
+
+        Scene escena = new Scene (vBox,1280,720);
+
+        Image logo = new Image("Images/logodehospital.png");
+        primaryStage.getIcons().add(logo);
+        primaryStage.setScene(escena);
+        primaryStage.setTitle("Hospital ");
+
+        primaryStage.show();
+        ;
+
+    }
+
+    private void EventosDoctores(int i){
+        switch (i){
+            case 1: new doctores(); break;
+        }
+    }
+
+    private void EventosPacientes(int i){
+        switch (i){
+            case 1: new pacientes(); break;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/sample/pacientes.java
@@ -0,0 +1,135 @@
+package sample;
+
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class pacientes extends Stage implements EventHandler {
+
+    private HBox hBox;
+    private VBox vBox;
+
+    private Label clave, nombre, sexo;
+    private Label cveNomina, nss, Cedula, salario;
+    private TextArea clavText, nombText, sexoText;
+    private TextArea cveNominaText, nssText, CedulaText, salarioText;
+    private Button Anadir;
+    private TableView tabla;
+
+    private Scene escena;
+
+
+    public pacientes() {
+        crearInterfaz();
+        this.setTitle("Doctores de Hospital ");
+        this.setScene(escena);
+
+        Image logo = new Image("Images/equipoMedico.png");
+
+        this.getIcons().add(logo);
+        this.show();
+
+    }
+
+    private void crearInterfaz() {
+        vBox = new VBox();
+        hBox = new HBox();
+
+        clave = new Label();
+        nombre = new Label();
+        sexo = new Label();
+        cveNomina = new Label();
+        nss = new Label();
+        Cedula = new Label();
+        salario = new Label();
+
+        clavText = new TextArea();
+        nombText = new TextArea();
+        sexoText = new TextArea();
+        cveNominaText = new TextArea();
+        nssText = new TextArea();
+        CedulaText = new TextArea();
+        salarioText = new TextArea();
+
+        clavText.setMaxSize(250, 1);
+        nombText.setMaxSize(250, 1);
+        sexoText.setMaxSize(250, 1);
+        cveNominaText.setMaxSize(250, 1);
+        nssText.setMaxSize(250, 1);
+        CedulaText.setMaxSize(250, 1);
+        salarioText.setMaxSize(250, 1);
+
+        clave.setText("Clave");
+        nombre.setText("Nombre");
+        sexo.setText("Sexo");
+        cveNomina.setText("Nomina");
+        nss.setText("nss");
+        Cedula.setText("Cedula");
+        salario.setText("Salario");
+
+        tabla = new TableView();
+        tabla.setMaxSize(650, 350);
+        tabla.setEditable(true);
+
+        TableColumn<doctores, String> claveTabla = new TableColumn("Clave");
+        claveTabla.setCellValueFactory(new PropertyValueFactory<>("Clave"));
+        tabla.getColumns().addAll(claveTabla);
+
+        TableColumn<doctores, String> nombreTabla = new TableColumn("Nombre");
+        nombreTabla.setCellValueFactory(new PropertyValueFactory<>("Clave"));
+        tabla.getColumns().addAll(nombreTabla);
+
+        TableColumn<doctores, String> sexoTabla = new TableColumn("Sexo");
+        sexoTabla.setCellValueFactory(new PropertyValueFactory<>("Sexo"));
+        tabla.getColumns().addAll(sexoTabla);
+
+        TableColumn<doctores, String> NominaTabla = new TableColumn("Nomina");
+        NominaTabla .setCellValueFactory(new PropertyValueFactory<>("Nomina"));
+        tabla.getColumns().addAll(NominaTabla );
+
+        TableColumn<doctores, String> nssTabla = new TableColumn("nss");
+        nssTabla .setCellValueFactory(new PropertyValueFactory<>("nss"));
+        tabla.getColumns().addAll(nssTabla );
+
+        TableColumn<doctores, String> cedulaTabla = new TableColumn("Cedula");
+        cedulaTabla.setCellValueFactory(new PropertyValueFactory<>("Cedula"));
+        tabla.getColumns().addAll(cedulaTabla);
+
+        TableColumn<doctores, String> salarioTabla = new TableColumn("Salario");
+        salarioTabla.setCellValueFactory(new PropertyValueFactory<>("Salario"));
+        tabla.getColumns().addAll(salarioTabla);
+
+        hBox.setPadding(new Insets(60));
+        //vBox.setPadding(new Insets(50));
+
+        Anadir = new Button();
+        Anadir.setText("Registrar Doctor");
+        Anadir.setPadding(new Insets(20));
+        Anadir.setMaxSize(160,10);
+
+        vBox.getChildren().addAll(clave, clavText, nombre, nombText, sexo, sexoText, cveNomina, cveNominaText, nss, nssText, Cedula, CedulaText, salario, salarioText, Anadir);
+        hBox.setPadding(new Insets(15));
+
+        hBox.getChildren().addAll(vBox, tabla);
+
+
+
+        escena = new Scene(hBox, 1280, 480);
+
+    }
+
+
+
+    @Override
+    public void handle(Event event) {
+    }
+}
